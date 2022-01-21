30a55b29e6b88ac2ea1cb2766ccd5e47602f43d1
Lawrence Chen <lawrencechen2002@gmail.com>
2022-01-21 15:47:27 -0800

    add rf pattern

---
+++ src/test/java/org/dataspread/sheetanalyzer/tacoTest/TestRFPattern.java
@@ -0,0 +1,76 @@
+package org.dataspread.sheetanalyzer.tacoTest;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import org.dataspread.sheetanalyzer.util.Ref;
+import org.dataspread.sheetanalyzer.util.RefImpl;
+import org.dataspread.sheetanalyzer.util.SheetNotSupportedException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.dataspread.sheetanalyzer.util.TestUtil;
+import org.dataspread.sheetanalyzer.SheetAnalyzer;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestRFPattern {
+
+  private static SheetAnalyzer sheetAnalyzer;
+    private static final String sheetName = "RFSheet";
+    private static final int maxRows = 1000;
+
+    private static File createRRSheet() throws IOException {
+        Workbook workbook = new HSSFWorkbook();
+        Sheet sheet = workbook.createSheet(sheetName);
+        int colA = 0, colB = 1, colC = 2;
+        for (int i = 0; i < maxRows; i++) {
+            Row row = sheet.createRow(i);
+            Cell cellA = row.createCell(colA);
+            Cell cellB = row.createCell(colB);
+            Cell cellC = row.createCell(colC);
+            cellA.setCellValue(i+1);
+            cellB.setCellValue(10);
+            cellC.setCellFormula("SUM(A" + (i + 1) + ":" + "B" + (maxRows) + ")");
+        }
+        TestUtil.createAnEmptyRowWithTwoCols(sheet, maxRows, colA, colB);
+
+        File xlsTempFile = TestUtil.createXlsTempFile();
+        FileOutputStream outputStream = new FileOutputStream(xlsTempFile);
+        FileOutputStream testStream = new FileOutputStream("test.xls");
+        workbook.write(testStream);
+        workbook.write(outputStream);
+        workbook.close();
+
+        return xlsTempFile;
+    }
+
+    @BeforeAll
+    public static void setUp() throws IOException, SheetNotSupportedException {
+        File xlsTempFile = createRRSheet();
+        boolean inRowCompression = false;
+        sheetAnalyzer = new SheetAnalyzer(xlsTempFile.getAbsolutePath(), inRowCompression);
+    }
+
+     @Test
+    public void verifyDependencyA() {
+        int queryRow = 1, queryColumn = 0;
+        Ref queryRef = new RefImpl(queryRow, queryColumn);
+        Set<Ref> queryResult = sheetAnalyzer.getDependents(sheetName, queryRef);
+
+        Set<Ref> groundTruth = new HashSet<>();
+        int firstRow = 0, firstColumn = 1;
+        int lastRow = 1, lastColumn = 1;
+        groundTruth.add(new RefImpl(firstRow, firstColumn, lastRow, lastColumn));
+        System.out.println(queryResult.toString());
+
+        Assertions.assertTrue(TestUtil.hasSameRefs(groundTruth, queryResult));
+    }
+}
