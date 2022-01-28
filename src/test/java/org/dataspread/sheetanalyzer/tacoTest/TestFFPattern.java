1fe08862d207c3a40ba3eded132bba6683701905
Lawrence Chen <lawrencechen2002@gmail.com>
2022-01-28 15:55:10 -0800

    add FF, FR, RR pattern tests

--- src/test/java/org/dataspread/sheetanalyzer/tacoTest/TestFFPattern.java
+++ src/test/java/org/dataspread/sheetanalyzer/tacoTest/TestFFPattern.java
@@ -5,14 +5,13 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-
+import org.dataspread.sheetanalyzer.SheetAnalyzer;
 import org.dataspread.sheetanalyzer.util.Ref;
 import org.dataspread.sheetanalyzer.util.RefImpl;
 import org.dataspread.sheetanalyzer.util.SheetNotSupportedException;
+import org.dataspread.sheetanalyzer.util.TestUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.dataspread.sheetanalyzer.util.TestUtil;
-import org.dataspread.sheetanalyzer.SheetAnalyzer;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -21,23 +20,24 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+public class TestFFPattern {
 
-public class TestRRPattern {
-
-    private static SheetAnalyzer sheetAnalyzer;
-    private static final String sheetName = "RRSheet";
+  private static SheetAnalyzer sheetAnalyzer;
+    private static final String sheetName = "FFSheet";
     private static final int maxRows = 1000;
 
-    private static File createRRSheet() throws IOException {
+    private static File createFFSheet() throws IOException {
         Workbook workbook = new HSSFWorkbook();
         Sheet sheet = workbook.createSheet(sheetName);
-        int colA = 0, colB = 1;
+        int colA = 0, colB = 1, colC = 2;
         for (int i = 0; i < maxRows; i++) {
             Row row = sheet.createRow(i);
             Cell cellA = row.createCell(colA);
             Cell cellB = row.createCell(colB);
-            cellA.setCellValue(1);
-            cellB.setCellFormula("SUM(A" + (i + 1) + ":" + "A" + (i + 2) + ")");
+            Cell cellC = row.createCell(colC);
+            cellA.setCellValue(i+1);
+            cellB.setCellValue(10);
+            cellC.setCellFormula("SUM(A1:" + "B" + maxRows + ")");
         }
         TestUtil.createAnEmptyRowWithTwoCols(sheet, maxRows, colA, colB);
 
@@ -51,49 +51,42 @@ public class TestRRPattern {
 
     @BeforeAll
     public static void setUp() throws IOException, SheetNotSupportedException {
-        File xlsTempFile = createRRSheet();
+        File xlsTempFile = createFFSheet();
         boolean inRowCompression = false;
         sheetAnalyzer = new SheetAnalyzer(xlsTempFile.getAbsolutePath(), inRowCompression);
     }
 
+    /**
+     * Every cell in column A is referenced by every cell in column C.
+     */
     @Test
     public void verifyDependencyA() {
-        int queryRow = 1, queryColumn = 0;
-        Ref queryRef = new RefImpl(queryRow, queryColumn);
+        int firstQueryRow = 0, firstQueryColumn = 0;
+        int lastQueryRow = maxRows - 1, lastQueryColumn = 0;
+        Ref queryRef = new RefImpl(firstQueryRow, firstQueryColumn, lastQueryRow, lastQueryColumn);
         Set<Ref> queryResult = sheetAnalyzer.getDependents(sheetName, queryRef);
 
         Set<Ref> groundTruth = new HashSet<>();
-        int firstRow = 0, firstColumn = 1;
-        int lastRow = 1, lastColumn = 1;
+        int firstRow = 0, firstColumn = 2;
+        int lastRow = maxRows - 1, lastColumn = 2;
         groundTruth.add(new RefImpl(firstRow, firstColumn, lastRow, lastColumn));
 
         Assertions.assertTrue(TestUtil.hasSameRefs(groundTruth, queryResult));
     }
 
+    /**
+     * Every cell in column B is referenced by every cell in column C.
+     */
     @Test
     public void verifyDependencyB() {
-        int queryRow = maxRows - 1, queryColumn = 0;
-        Ref queryRef = new RefImpl(queryRow, queryColumn);
+        int firstQueryRow = 0, firstQueryColumn = 1;
+        int lastQueryRow = maxRows - 1, lastQueryColumn = 1;
+        Ref queryRef = new RefImpl(firstQueryRow, firstQueryColumn, lastQueryRow, lastQueryColumn);
         Set<Ref> queryResult = sheetAnalyzer.getDependents(sheetName, queryRef);
 
         Set<Ref> groundTruth = new HashSet<>();
-        int firstRow = maxRows - 2, firstColumn = 1;
-        int lastRow = maxRows - 1, lastColumn = 1;
-        groundTruth.add(new RefImpl(firstRow, firstColumn, lastRow, lastColumn));
-        System.out.println(queryResult);
-
-        Assertions.assertTrue(TestUtil.hasSameRefs(groundTruth, queryResult));
-    }
-
-    @Test
-    public void verifyDependencyC() {
-        int queryRow = 0, queryColumn = 0;
-        Ref queryRef = new RefImpl(queryRow, queryColumn);
-        Set<Ref> queryResult = sheetAnalyzer.getDependents(sheetName, queryRef);
-
-        Set<Ref> groundTruth = new HashSet<>();
-        int firstRow = 0, firstColumn = 1;
-        int lastRow = 0, lastColumn = 1;
+        int firstRow = 0, firstColumn = 2;
+        int lastRow = maxRows - 1, lastColumn = 2;
         groundTruth.add(new RefImpl(firstRow, firstColumn, lastRow, lastColumn));
 
         Assertions.assertTrue(TestUtil.hasSameRefs(groundTruth, queryResult));
